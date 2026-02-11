bbaffd46f670a8e727442447ea022b335dab9545
Duarte Mechas <duartemechas@gmail.com>
2026-02-11 11:32:08 +0000

    some changes that i don't get

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+map("n", "<S-F12>", function()
+  Snacks.picker.lsp_references()
+end, { desc = "Goto reference" })
+map("n", "<F12>", function()
+  Snacks.picker.lsp_definitions()
+end, { desc = "Goto definition" })
